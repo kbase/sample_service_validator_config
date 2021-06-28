ab7020bfa3fd4ce9e478554661707baed4270614
Erik Pearson <eapearson@lbl.gov>
2021-06-28 10:32:03 -0700

    recommended formatting fixes.

--- scripts/merge_validators.py
+++ scripts/merge_validators.py
@@ -3,7 +3,6 @@ import sys
 import yaml
 from copy import deepcopy
 
-
 ONTOLOGY_MAPPING = {
     "envo_ontology": "ENVO_terms",
     "go_ontology": "GO_terms"
@@ -13,23 +12,26 @@ _BUILTIN = 'SampleService.core.validator.builtin'
 _NOOP = [{
     'callable_builder': 'noop',
     'module': _BUILTIN
-    }]
+}]
+
 
 def find_ontology_validator(data_field, key, ontology_validators):
-#    if data_field[key].get('validators'):
-#        for validator in data_field[key]['validators']:
+    #    if data_field[key].get('validators'):
+    #        for validator in data_field[key]['validators']:
     for validator in data_field['validators']:
-            if validator['callable_builder'] == 'ontology_has_ancestor':
-                if not ontology_validators.get(key):
-                    ontology_validators[key] = []
-                ontology_validators[key].append({
-                    "validator_type": "ontology_has_ancestor",
-                    "ontology": validator.get('parameters', {}).get('ontology'),
-                    "ontology_collection": ONTOLOGY_MAPPING.get(validator.get('parameters', {}).get('ontology'), ""),
-                    "ancestor_term": validator.get('parameters', {}).get('ancestor_term'),
-                    "display_name": data_field['key_metadata']['title']
-                })
+        if validator['callable_builder'] == 'ontology_has_ancestor':
+            if not ontology_validators.get(key):
+                ontology_validators[key] = []
+            ontology_validators[key].append({
+                "validator_type": "ontology_has_ancestor",
+                "ontology": validator.get('parameters', {}).get('ontology'),
+                "ontology_collection": ONTOLOGY_MAPPING.get(
+                    validator.get('parameters', {}).get('ontology'), ""),
+                "ancestor_term": validator.get('parameters', {}).get('ancestor_term'),
+                "display_name": data_field['key_metadata']['title']
+            })
     return ontology_validators
+
 
 def expand_validators(val):
     nv = []
@@ -39,20 +41,16 @@ def expand_validators(val):
         raise ValueError("Missing type")
     typ = val['type']
     if 'units' in val:
-        v = {
-            'callable_builder': 'units',
-            'module': _BUILTIN,
-            }
-        v['parameters'] = {
-                'key': 'units',
-                'units': val['units']
-                }
+        v = {'callable_builder': 'units', 'module': _BUILTIN, 'parameters': {
+            'key': 'units',
+            'units': val['units']
+        }}
         nv.append(v)
     v = {
         'callable_builder': typ,
         'module': _BUILTIN,
-        }
-    if typ=='number':
+    }
+    if typ == 'number':
         v['parameters'] = {'keys': 'value'}
         if 'maximum' in val:
             v['parameters']['lte'] = val['maximum']
@@ -66,11 +64,11 @@ def expand_validators(val):
     elif 'ontology' in val:
         v['callable_builder'] = 'ontology_has_ancestor'
         v['parameters'] = {
-                'ontology': val['ontology']['ns'],
-                'ancestor_term': val['ontology']['ancestor_term']
-                }
+            'ontology': val['ontology']['ns'],
+            'ancestor_term': val['ontology']['ancestor_term']
+        }
     elif 'max-len' in val:
-             v['parameters'] = {'max-len': val['max-len']}
+        v['parameters'] = {'max-len': val['max-len']}
     elif typ == 'string':
         return deepcopy(_NOOP)
     else:
@@ -91,11 +89,10 @@ def merge_to_existing_validators(val_type, val_data, key, val, data):
                 else:
                     val_data[key][data_field] = data[val_type][key][data_field]
     else:
-        nv = {'key_metadata': {}}
-        nv['validators'] = expand_validators(val)
+        nv = {'key_metadata': {}, 'validators': expand_validators(val)}
         for k in val:
             nv['key_metadata'][k] = val[k]
-           
+
         val_data[key] = deepcopy(nv)
     return val_data
 
@@ -113,7 +110,7 @@ def merge_validation_files(files, output_file, ontology_file):
         prefix = ''
         if 'namespace' in data:
             prefix = data['namespace'] + ":"
-        for val_type, val_data in [ ('terms', validators) ]:
+        for val_type, val_data in [('terms', validators)]:
             if data.get(val_type):
                 for key, val in data[val_type].items():
                     keyname = prefix + key
@@ -123,7 +120,7 @@ def merge_validation_files(files, output_file, ontology_file):
                     ontology_validators = find_ontology_validator(
                         val_data[keyname], keyname, ontology_validators
                     )
-#                        data[val_type], key, ontology_validators
+    #                        data[val_type], key, ontology_validators
 
     v_keys = sorted(list(validators.keys()))
     pv_keys = sorted(list(prefix_validators.keys()))
@@ -135,7 +132,7 @@ def merge_validation_files(files, output_file, ontology_file):
         data['prefix_validators'] = prefix_validators
     # try default_style with quotes here.
     with open(output_file, 'w') as f:
-        yaml.dump(data, f)  #, default_style='"')
+        yaml.dump(data, f)  # , default_style='"')
     # save ontology_file
     with open(ontology_file, 'w') as f:
         yaml.dump(ontology_validators, f)  # , default_stype='"')
