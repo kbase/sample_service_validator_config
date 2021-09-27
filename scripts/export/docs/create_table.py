e699f671500b78c32df9f46e2896196fceb70da3
Erik Pearson <eapearson@lbl.gov>
2021-09-27 16:43:06 -0700

    Add internal stylesheet; partially copy styles from doc site (wip).

--- scripts/export/docs/create_table.py
+++ scripts/export/docs/create_table.py
@@ -2,6 +2,7 @@ import sys
 import yaml
 import pandas as pd
 import codecs
+import uuid
 
 
 def get_unit(validator_spec, default_value=""):
@@ -39,39 +40,27 @@ def get_ontology_constraints(validator_spec, default_value={}):
     return default_value
 
 
-def dict_to_table(value):
-    table_style = "width: 100%;"
-    th_style = (
-        "text-align: left; font-style: italic; font-weight: normal; padding: 2px;"
-    )
-    td_style = "margin: 0; padding: 2px;  "
-    html = f'<table cellspacing="0" style="{table_style}">'
+def dict_to_table(value, style_id):
+    html = f'<table class="Dict-{style_id}">'
 
     html += "<tbody>"
     for key, value in value.items():
         html += "<tr>"
-        html += f'<th style="{th_style}">{key}</th>'
-        html += f'<td style="{td_style}">{value}</td>'
+        html += f"<th >{key}</th>"
+        html += f"<td >{value}</td>"
         html += "</tr>"
     html += "</tbody></table>"
 
     return html
 
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: create_table.py <file-to-transform> <output-directory>")
-        sys.exit(1)
-
-    input_file = sys.argv[1]
-    output_dir = sys.argv[2]
-
+def create_table(input_file, style_id):
     with open(input_file) as f:
         metadata_validation = yaml.load(f, Loader=yaml.SafeLoader)
 
     table_columns = [
-        {"key": "key", "label": "Sample Key"},
-        {"key": "label", "label": "Label"},
+        {"key": "key", "label": "Field Key"},
+        {"key": "label", "label": "Field Label"},
         {"key": "description", "label": "Description"},
         {"key": "examples", "label": "Example(s)"},
         {"key": "type", "label": "Type"},
@@ -106,29 +95,119 @@ def main():
                 f"<div>{examples}</div>",
                 field_type,
                 unit,
-                dict_to_table(constraints),
+                dict_to_table(constraints, style_id),
             ]
         )
 
-    # Generate an html table the cheesy way:
-    # table_style = ""
-    th_style = "margin: 0; padding: 4px; border: 1px solid rgba(200, 200, 200, 1); "
-    td_style = "margin: 0; padding: 4px; border: 1px solid rgba(200, 200, 200, 1); "
-
-    html = '<table cellspacing="0">'
-
-    html += "<thead>"
-
+    table_head = ""
     for col in table_columns:
-        html += f'<th style="{th_style}">{col["label"]}</th>'
-    html += "</thead>"
+        table_head += f'<th>{col["label"]}</th>'
 
-    html += "<tbody>"
+    table_body = ""
     for row in rows:
-        html += "<tr>"
+        table_body += "<tr>\n"
         for col in row:
-            html += f'<td style="{td_style}">{col}</td>'
-    html += "</tbody></table>"
+            table_body += f"<td>{col}</td>\n"
+        table_body == "</tr>\n"
+
+    table_html = """
+<table class="Validators-{style_id}">
+    <thead>
+        <tr>
+            {table_head}
+        </tr>
+    </thead>
+    <tbody>
+    {table_body}
+    </tbody>
+</table>
+"""
+    return table_html.format(
+        table_head=table_head, table_body=table_body, style_id=style_id
+    )
+
+
+def create_stylesheet(style_id):
+    stylesheet = """
+<style>
+table.Validators-{style_id} {{
+    width: 100%;
+    font-size: 16px;
+    font-family: Content-font, Roboto, sans-serif;
+    font-weight: 400;
+    line-height: 1.625;
+    border-collapse: collapse;
+}}
+table.Validators-{style_id} > thead > tr {{
+    border-bottom: 1px solid rgb(230, 236, 241);
+}}
+table.Validators-{style_id} > thead > tr > th,
+table.Validators-{style_id} > tbody > tr > td {{
+    margin: 0; 
+    padding: 6px;
+    border: 1px solid rgb(200, 200, 200);
+    vertical-align: top;
+}}
+
+table.Validators-{style_id} > thead > tr > th {{
+    font-weight: 700;
+    color: rgb(157, 170, 182);
+}}
+
+
+table.Dict-{style_id} {{
+    width: 100%;
+    font-size: 14px;
+    font-family: Content-font, Roboto, sans-serif;
+    font-weight: 400;
+    line-height: 1.625;
+    border-collapse: collapse;
+}}
+table.Dict-{style_id} > thead > tr {{
+    border-bottom: 1px solid rgb(230, 236, 241);
+}}
+table.Dict-{style_id} > tbody > tr > th,
+table.Dict-{style_id} > tbody > tr > td {{
+    margin: 0; 
+    padding: 2px;
+    vertical-align: top;
+}}
+
+table.Dict-{style_id} > tbody > tr > td {{
+    font-family: monospace;
+    white-space: pre;
+}}
+
+table.Dict-{style_id} > tbody > tr > th {{
+    text-align: right; 
+    font-style: italic; 
+    font-weight: normal; 
+    color: rgb(157, 170, 182);
+}}
+
+table.Dict-{style_id} > tbody > tr > th::after {{
+    content: ":"
+}}
+</style>
+"""
+    return stylesheet.format(style_id=style_id)
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: create_table.py <file-to-transform> <output-directory>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    style_id = str(uuid.uuid4())
+    print("style id", style_id)
+
+    html_table = create_table(input_file, style_id)
+    table_stylesheet = create_stylesheet(style_id)
+
+    html = f'<div class="">{table_stylesheet}{html_table}</div>'
 
     with codecs.open(f"{output_dir}/sample_fields.html", "w", "utf-8-sig") as f:
         f.write(html)
