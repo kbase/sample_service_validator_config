d58a4ef999262d55863f89499238e051bdf08c40
Erik Pearson <eapearson@lbl.gov>
2021-09-27 22:18:05 -0700

    improve table formatting; create grouped alts

--- scripts/automation/create_validators_table.sh
+++ scripts/automation/create_validators_table.sh
@@ -1,1 +1,3 @@
 python3 scripts/export/docs/create_table.py `pwd`/dist/metadata_validation.yml dist
+python3 scripts/export/docs/create_table_grouped.py `pwd`/dist/metadata_validation.yml `pwd`/dist/groups.json dist
+python3 scripts/export/docs/create_table_grouped_panel.py `pwd`/dist/metadata_validation.yml `pwd`/dist/groups.json dist
--- scripts/export/docs/create_table_grouped.py
+++ scripts/export/docs/create_table_grouped.py
@@ -1,5 +1,6 @@
 import sys
 import yaml
+import json
 import pandas as pd
 import codecs
 import uuid
@@ -53,9 +54,40 @@ def dict_to_table(value, style_id):
     return html
 
 
-def create_table(input_file, style_id, sort_by_column=0):
+def validator_to_row(key, validator, style_id):
+    field_type = validator["key_metadata"].get("type", "string")
+    examples = "".join(
+        [
+            f"<li><code>{str(example)}</code></li>"
+            for example in validator["key_metadata"].get("examples", [])
+        ]
+    )
+    unit = get_unit(validator)
+    constraints = {}
+    if field_type == "number":
+        constraints = get_number_constraints(validator)
+    elif field_type == "string":
+        if validator["key_metadata"].get("format", None) == "ontology-term":
+            unit = "ontology_term"
+            constraints = get_ontology_constraints(validator)
+
+    return [
+        key,
+        validator["key_metadata"]["title"],
+        validator["key_metadata"]["description"],
+        f'<ul class="Examples-{style_id}">{examples}</ul>',
+        field_type,
+        unit,
+        dict_to_table(constraints, style_id),
+    ]
+
+
+def create_table(input_file, grouping_file, style_id, sort_by_column=0):
     with open(input_file) as f:
         metadata_validation = yaml.load(f, Loader=yaml.SafeLoader)
+
+    with open(grouping_file) as f:
+        grouping = json.load(f)
 
     table_columns = [
         {"key": "key", "label": "Field Key"},
@@ -69,46 +101,65 @@ def create_table(input_file, style_id, sort_by_column=0):
 
     validators = metadata_validation["validators"]
     # f"<div style='margin-bottom: 4px; display: flex; flex-direction: row;'><div style='flex: 0 0 1em; color: gray;'>⦁</div><div style='1 1 0;'><code>{str(example)}</code></div></div>"
-    rows = []
-    for key, validator in validators.items():
-        field_type = validator["key_metadata"].get("type", "string")
-        examples = "".join(
-            [
-                f"<li><code>{str(example)}</code></li>"
-                for example in validator["key_metadata"].get("examples", [])
-            ]
-        )
-        unit = get_unit(validator)
-        constraints = {}
-        if field_type == "number":
-            constraints = get_number_constraints(validator)
-        elif field_type == "string":
-            if validator["key_metadata"].get("format", None) == "ontology-term":
-                unit = "ontology_term"
-                constraints = get_ontology_constraints(validator)
+    # rows = []
+    # for key, validator in validators.items():
+    #     field_type = validator["key_metadata"].get("type", "string")
+    #     examples = "".join(
+    #         [
+    #             f"<li><code>{str(example)}</code></li>"
+    #             for example in validator["key_metadata"].get("examples", [])
+    #         ]
+    #     )
+    #     unit = get_unit(validator)
+    #     constraints = {}
+    #     if field_type == "number":
+    #         constraints = get_number_constraints(validator)
+    #     elif field_type == "string":
+    #         if validator["key_metadata"].get("format", None) == "ontology-term":
+    #             unit = "ontology_term"
+    #             constraints = get_ontology_constraints(validator)
 
-        rows.append(
-            [
-                key,
-                validator["key_metadata"]["title"],
-                validator["key_metadata"]["description"],
-                f'<ul class="Examples-{style_id}">{examples}</ul>',
-                field_type,
-                unit,
-                dict_to_table(constraints, style_id),
-            ]
-        )
+    #     rows.append(
+    #         [
+    #             key,
+    #             validator["key_metadata"]["title"],
+    #             validator["key_metadata"]["description"],
+    #             f'<ul class="Examples-{style_id}">{examples}</ul>',
+    #             field_type,
+    #             unit,
+    #             dict_to_table(constraints, style_id),
+    #         ]
+    #     )
 
     table_head = ""
     for col in table_columns:
         table_head += f'<th>{col["label"]}</th>'
 
     table_body = ""
-    for row in sorted(rows, key=lambda kv: kv[1][sort_by_column]):
-        table_body += "<tr>\n"
-        for col in row:
-            table_body += f"<td>{col}</td>\n"
-        table_body == "</tr>\n"
+
+    grouped_rows = []
+    for group in grouping:
+        rows = []
+        for field_key in group["fields"]:
+            validator = validators[field_key]
+            row = validator_to_row(field_key, validator, style_id)
+            rows.append(row)
+        grouped_rows.append({"group": group, "rows": rows})
+
+    table_body = ""
+    for group in grouped_rows:
+        table_body += f'<tr class="-groupTitle"><td colspan="7">{group["group"]["title"]}</td></tr>\n'
+        for row in group["rows"]:
+            table_body += "<tr>"
+            for col in row:
+                table_body += f"<td>{col}</td>"
+            table_body += "</tr>\n"
+
+    # for row in sorted(rows, key=lambda kv: kv[1][sort_by_column]):
+    #     table_body += "<tr>\n"
+    #     for col in row:
+    #         table_body += f"<td>{col}</td>\n"
+    #     table_body == "</tr>\n"
 
     table_html = """
 <table class="Validators-{style_id}">
@@ -152,6 +203,13 @@ table.Validators-{style_id} > tbody > tr > td {{
 table.Validators-{style_id} > thead > tr > th {{
     font-weight: 700;
     color: rgb(157, 170, 182);
+}}
+
+table.Validators-{style_id} tr.-groupTitle {{
+    font-weight: bold;
+    font-size: 150%;
+    color: rgb(157, 170, 182);
+    margin-top: 20px;
 }}
 
 
@@ -209,21 +267,24 @@ ul.Examples-{style_id} > li::before {{
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: create_table.py <file-to-transform> <output-directory>")
+    if len(sys.argv) != 4:
+        print(
+            "Usage: create_table.py <file-to-transform> <groupings> <output-directory>"
+        )
         sys.exit(1)
 
     input_file = sys.argv[1]
-    output_dir = sys.argv[2]
+    grouping_file = sys.argv[2]
+    output_dir = sys.argv[3]
 
     style_id = str(uuid.uuid4())
 
-    html_table = create_table(input_file, style_id, 1)
+    html_table = create_table(input_file, grouping_file, style_id, 1)
     table_stylesheet = create_stylesheet(style_id)
 
     html = f'<div class="">{table_stylesheet}{html_table}</div>'
 
-    with codecs.open(f"{output_dir}/sample_fields.html", "w", "utf-8-sig") as f:
+    with codecs.open(f"{output_dir}/sample_fields_grouped.html", "w", "utf-8-sig") as f:
         f.write(html)
 
     print(f"    created table  {input_file} written to {output_dir}.")
